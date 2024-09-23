be760c638b4b4874690f9d949b923dfa52ef7175
dhj <donghaoji@csu.ac.cn>
2024-09-23 17:03:01 +0800

    2024092303

--- DataModule/CIFAR10ImbalanceDataModule.py
+++ DataModule/CIFAR10ImbalanceDataModule.py
@@ -2,19 +2,52 @@ import torch
 from pytorch_lightning import LightningDataModule
 import numpy as np
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS
-from torch.utils.data import Dataset, Subset, DataLoader
+from torch.utils.data import Dataset, Subset, DataLoader, Sampler
 from torchvision import datasets, transforms
+
+# 自定义采样器
+class CIFAR10Sampler(Sampler):
+    """
+    自定义采样器，每一次从数据中取nt个尾部数据 nt(1+na)个头部数据
+    """
+    def __init__(self, tail_indices, head_indices, nt, na):
+        self.tail_indices = tail_indices
+        self.head_indices = head_indices
+        self.nt = nt
+        self.na = na
+        self.num_batches = len(self.tail_indices) // self.nt  # 批次数
+
+    def __iter__(self):
+        np.random.shuffle(self.tail_indices)
+        np.random.shuffle(self.head_indices)
+
+        for i in range(self.num_batches):
+            # 从尾部采样nt个数据
+            tail_batch = self.tail_indices[i * self.nt:(i + 1) * self.nt]
+
+            # 从头部数据采样nt(1+na)个数据
+            head_start_idx = i * self.nt * (1 + self.na)
+            head_end_idx = (i + 1) * self.nt * (1 + self.na)
+            head_batch = self.head_indices[head_start_idx:head_end_idx]
+
+            # 组合为一个batch
+            yield list(tail_batch) + list(head_batch)
+
+    def __len__(self):
+        return self.num_batches
 
 
 # 设定类别0 1 2 3 4为尾部类
 # 构建CIFAR10的尾部类数据
 class CIFAR10ImbalanceDataModule(LightningDataModule):
-    def __init__(self, batch_size, tail_classes, tail_ratio=0.1, data_dir='./data'):
+    def __init__(self, batch_size, tail_classes, tail_ratio=0.1, data_dir='./data', nt=3, na=3):
         super().__init__()
         self.data_dir = data_dir
         self.tail_classes = tail_classes # [0, 1, 2, 3, 4]
         self.tail_ratio = tail_ratio
         self.batch_size = batch_size
+        self.nt = nt
+        self.na = na
 
         # 数据变换
         self.transform = transforms.Compose([
@@ -37,8 +70,8 @@ class CIFAR10ImbalanceDataModule(LightningDataModule):
         val_dataset = datasets.CIFAR10(root=self.data_dir, train=False, transform=self.transform)
 
         # 根据给定的尾部类和头部类构建不平衡的训练集
-        tail_class_indices = []  # 保存尾部类样本的索引
-        head_class_indices = []  # 保存头部类样本的索引
+        self.tail_class_indices = []  # 保存尾部类样本的索引
+        self.head_class_indices = []  # 保存头部类样本的索引
 
         for tail_class in self.tail_classes:
             # 获取当前尾部类的所有样本索引
@@ -46,16 +79,16 @@ class CIFAR10ImbalanceDataModule(LightningDataModule):
             # 按比例随机采样该类别的样本索引
             num_samples = int(len(class_indices) * self.tail_ratio)
             sample_indices = np.random.choice(class_indices, num_samples, replace=False)
-            tail_class_indices.extend(sample_indices)
+            self.tail_class_indices.extend(sample_indices)
 
         for head_class in range(10):
             if head_class not in self.tail_classes:
                 # 获取当前头部类的所有样本索引
                 class_indices = np.where(np.array(full_train_dataset.targets) == head_class)[0]
-                head_class_indices.extend(class_indices)
+                self.head_class_indices.extend(class_indices)
 
         # 组合尾部类和头部类的索引，创建不平衡数据集
-        imbalanced_indices = np.array(tail_class_indices + head_class_indices)
+        imbalanced_indices = np.array(self.tail_class_indices + self.head_class_indices)
         np.random.shuffle(imbalanced_indices)  # 打乱样本索引
 
         # 创建不平衡数据集
@@ -63,10 +96,11 @@ class CIFAR10ImbalanceDataModule(LightningDataModule):
         self.test_datasets = val_dataset
 
     def train_dataloader(self):
-        return DataLoader(self.train_datasets, batch_size=self.batch_size, shuffle=True)
+        sampler = CIFAR10Sampler(self.tail_class_indices, self.head_class_indices, self.nt, self.na)
+        return DataLoader(self.train_datasets, batch_size=self.nt + self.nt * (1 + self.na), sampler=sampler)
 
     def val_dataloader(self):
-        return DataLoader(self.test_datasets, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(self.test_datasets, batch_size=self.nt + self.nt * (1 + self.na), shuffle=False)
 
 
 
--- main.py
+++ main.py
@@ -62,18 +62,24 @@ class CIFAR10Classifier(LightningModule):
 
 
 class TwoStageCIFAR10Classifier(LightningModule):
-    def __init__(self, _args, _sim_geometric, _sorted_eigenvalues_list, _sorted_eigenvectors_list):
+    def __init__(self, _args, _sim_geometric, _sorted_eigenvalues_list, _sorted_eigenvectors_list, tail_classes):
         super(TwoStageCIFAR10Classifier, self).__init__()
         self.args = _args
         self.sim_geometric = _sim_geometric
         self.sorted_eigenvalues_list = _sorted_eigenvalues_list
         self.sorted_eigenvectors_list = _sorted_eigenvectors_list
+        self.tail_classes = tail_classes
 
         self.feature_extractor = CIFAR10Classifier().model
         self.classifer = CIFAR10Classifier().classifer
 
     def forward(self, x, stage=0):
-        pass
+        # 确定训练阶段
+        self.freeze_diff_stage(stage)
+        # 前馈过程
+        logits = self.feature_extractor(x)
+        output = self.classifer(logits)
+        return output, logits
 
     def freeze_diff_stage(self, stage=0):
         if stage == 0:
@@ -89,6 +95,28 @@ class TwoStageCIFAR10Classifier(LightningModule):
                 param.requires_grad = True
             for param in self.classifer.parameters():
                 param.requires_grad = False
+
+    # 不确定性表示函数
+    def feature_uncertainty_representation(self, x, index):
+        """
+           根据特征向量和特征值，对给定特征进行不确定性表示
+        """
+        perturbed_features = []
+        for _ in range(self.args.na):
+            epsilon = np.random.normal(0, 1, self.sorted_eigenvalues_list[index].shape)
+            perturbed_value = np.dot(self.sorted_eigenvectors_list[index], epsilon * self.sorted_eigenvalues_list[index])
+            perturbed_feature = x + perturbed_value
+            perturbed_features.append(perturbed_feature)
+        return perturbed_features
+
+
+    def training_step(self, batch, batch_idx):
+        images, labels = batch
+        # 获取原始logits
+        for image, label in zip(images, labels):
+            with torch.no_grad():
+                original_logits = self.feature_extractor(image)
+            
 
 
 def train(args):
@@ -119,8 +147,15 @@ if __name__ == '__main__':
     parse.add_argument("--train", default=False, type=bool, help="whether is train process")
     parse.add_argument("--checkpoint", default='./checkpoint', type=str, help="checkpoint path")
     parse.add_argument("--num_classes", default=10, type=int, help="number of classes")
+    parse.add_argument("--nt", default=3, type=int, help="nt for tail_num")
+    parse.add_argument("--na", default=2, type=int, help="na for each tail class")
 
     args = parse.parse_args()
+    # 尾部类
+    tail_classes = [0, 1, 2, 3, 4]
+    # 构建batch_size
+    args.batch_size = args.nt + args.nt * (1 + args.na)
+    print(f"batch_size: {args.batch_size}")
 
     cifar_transforms = transforms.Compose([
         transforms.ToTensor(),
@@ -173,20 +208,30 @@ if __name__ == '__main__':
         average_avg_scores = class_logits.mean(dim=0)
         average_scores[c] = average_avg_scores
 
-    # 寻找最相似的类别（除了当前类别之外，平均logit分数最大的类别）
+    # 寻找最相似的类别（除了当前类别之外，平均logit分数最大的别的非当前头（尾部）类别）
     most_sim_classes = []
     for c in range(args.num_classes):
         avg_scores_c = average_scores[c]
         avg_scores_c[c] = float('-inf')# 当前类别置为负无穷大，不参与比较
+        if c in tail_classes:
+            # c是尾部类，将所有尾部类设置为负无穷大 不参与比较
+            for c_idx in range(args.num_classes):
+                if c_idx in tail_classes:
+                    avg_scores_c[c_idx] = float('-inf')
+        else:
+            # c是头部类，将所有的头部类都设置为负无穷大 不参与比较
+            for c_idx in range(args.num_classes):
+                if c_idx not in tail_classes:
+                    avg_scores_c[c_idx] = float('-inf')
         most_similar_class = torch.argmax(avg_scores_c).item()# 最相似类别
         most_sim_classes.append(most_similar_class)
         print(f"Class {c} is most similar to class {most_similar_class}")
 
     print("Most similar classes for each class:", most_sim_classes)
 
-    # 相似类的logits集合 后续利用它们指导尾部类的分布恢复
+    # 相似类的logits集合 后续利用它们指导尾部类的分布恢复 如果是头部类 则0 是头部类 1 是尾部类；
+    # 如果是尾部类 则0 是尾部类 1是头部类
     sim_logits_matrix = [] # [[shape(m, 128), shape(n, 128)],...]
-
     for c in range(args.num_classes):
         class_mask = (label_all_tensor == c)
         class_sim_mask = (label_all_tensor == most_sim_classes[c])
@@ -218,13 +263,13 @@ if __name__ == '__main__':
         src_sorted_indices = np.argsort(src_eigenvalues)[::-1]
         src_eigenvalues = src_eigenvalues[src_sorted_indices]
         src_eigenvectors = src_eigenvectors[:, src_sorted_indices]
-        # 存储排序以后的特征值和特征向量
-        sorted_eigenvalues_list.append(src_eigenvalues)
-        sorted_eigenvectors_list.append(src_eigenvectors)
 
         src_sorted_indices = np.argsort(tar_eigenvalues)[::-1]
         tar_eigenvalues = tar_eigenvalues[src_sorted_indices]
         tar_eigenvectors = tar_eigenvectors[:, src_sorted_indices]
+        # 存储排序以后的特征值和特征向量 主要面向 尾部类 -》头部类
+        sorted_eigenvalues_list.append(tar_eigenvalues)
+        sorted_eigenvectors_list.append(tar_eigenvectors)
 
         print("sorted")
         similarity = 0
